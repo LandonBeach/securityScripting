adf33dd6d713f8cf872a6dfa2f1fb94c0fc6380d
LandonBeach <LandonBeach@users.noreply.github.com>
2017-03-07 09:50:31 -0700

    Update file_utils.py

--- file_utils.py
+++ file_utils.py
@@ -1,20 +1,25 @@
-def file_io(file_name, mode, *text):
+# Landon Beach
+# 3/8/17
+
+def file_io(file_name, mode, *text=None):
 	mode = mode.lower()
 	# Open the file
-	with open(file_name) as f:
+	with open(file_name, mode) as f:
 		# Use the mode to determine how to handle the file.
-		if (mode == 'r'):
+		if mode == 'r':
 			# Print the contents to stdout.
-			pass
-		elif (mode == 'r+'):
+			print(f.read())
+		elif mode == 'r+':
 			# Determine if the user sent a list of strings to write.
 			# If the user sent lines to write, write the lines (each on a new line)
+			if text is not None:
+				for t in text:
+					f.write(t + "\n")
 			# If not, read the lines of the file and print them to stdout.
-			pass
-		elif (mode == 'a' or mode == 'w'):
+			else:
+				print(f.read())
+		elif mode == 'a' or mode == 'w':
 			# Write each string to a new line in the file.
-			pass
-		else:
-			# Invalid mode
-			pass
-
+			if text is not None:
+				for t in text:
+					f.write(t + "\n")
