aa5a6faad0317b0759500d23f9b5862d97dcc6eb
Landon Beach <LandonBeach@users.noreply.github.com>
2017-04-05 10:59:12 -0600

    Add files via upload

--- parrot_client.py
+++ parrot_client.py
@@ -1,16 +1,32 @@
-import socket, sys
+# This is a simple client that sends a message to a server.
+# Author: Landon Beach
+# Date: 4/5/17
+import socket, optparse
 
+# Parse the arguments and assign it to the variable "options".
+parser = optparse.OptionParser()
+parser.add_option("-s", "--server", default="127.0.0.1", dest="svr_addr", help="Connect to IP address SERVER_IP", metavar="SERVER_IP")
+parser.add_option("-p", "--port", default=4444, type="int", dest="svr_port", help="Connect to port SERVER_PORT", metavar="SERVER_PORT")
+parser.add_option("-m", "--message", default=" ", dest="message", help="Send MESSAGE to server", metavar="MESSAGE")
+(options, args) = parser.parse_args()
+
+# Connect to the server IP and port given from the arguments.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-svr_addr = ("localhost", 4444)
+svr_addr = (options.svr_addr, options.svr_port)
 print("Connecting to %s on port %s... " % (svr_addr[0], svr_addr[1]), end='')
 sock.connect(svr_addr)
 print("Connected!")
 
 
 try:
-	msg = bytes(input("Please enter a message to send: "), "UTF-8")
+	# Send the message from the arguments to the server.
+	msg = bytes(options.message, "UTF-8")
 	sock.sendall(msg)
+	
+	# Receive and print the data from the server.
 	data = sock.recv(1024)
 	print("Received data: %s" % data.decode())
+	
 finally:
+	# Close the socket to the server.
 	sock.close()
--- parrot_server.py
+++ parrot_server.py
@@ -1,18 +1,34 @@
-import socket, sys
+# This is a server that sends back the data that it receives.
+# Author: Landon Beach
+# Date: 4/5/17
 
+import socket, optparse
+
+# Parse the arguments and assign it to the variable "options".
+parser = optparse.OptionParser()
+parser.add_option("-s", "--server", default="127.0.0.1", dest="svr_addr", help="Bind to IP address SERVER_IP", metavar="SERVER_IP")
+parser.add_option("-p", "--port", default=4444, type="int", dest="svr_port", help="Bind to port SERVER_PORT", metavar="SERVER_PORT")
+(options, args) = parser.parse_args()
+
+# Create a socket using the arguments given.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-svr_addr = ("localhost", 4444)
+svr_addr = (options.svr_addr, options.svr_port)
 
 try:
+	# Bind to the socket and start a listener.
 	sock.bind(svr_addr)
 	sock.listen(1)
 	print("Listening on %s on port %d." % (svr_addr[0], svr_addr[1]))
 	
+	# Wait for a connection to the socket listener.
 	connection, client_addr = sock.accept()
 	print("Connection from %s" % client_addr[0])
 
 	while True:
+		# Receive data from the connected client.
 		raw_data = connection.recv(1024)
+		
+		# If there is data, print it and send it back to the client.
 		if raw_data:
 			data = raw_data.decode()
 			print("[Received message from %s] %s" % (client_addr[0], data))
@@ -22,4 +38,5 @@ try:
 			break
 	
 finally:
+	# Close the connection to the client.
 	connection.close()
